a6196cd7dfd11c62ad7982e5d48273d8cdee0939
Adhiana <adhianamastur48@gmail.com>
2022-12-24 23:22:57 +0700

    fix port

--- server/server.js
+++ server/server.js
@@ -44,6 +44,7 @@ app.post("/", async (req, res) => {
     }
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
