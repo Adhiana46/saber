e468cc95d0b57dfa73f8059013a1a22745be15bb
Adhiana <adhianamastur48@gmail.com>
2022-12-24 22:34:18 +0700

    openai server

---
+++ server/server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from 'cors';
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).send({
+        message: "Hello from Saber",
+    });
+});
+
+app.post("/", async (req, res) => {
+    try {
+        const { prompt } = req.body;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        });
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error });
+    }
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
