66c024808fce1ad59c0d933ea6ab182c35e193e8
Adhiana <adhianamastur48@gmail.com>
2022-12-24 23:26:36 +0700

    update api url

--- client/main.js
+++ client/main.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from openai server
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://saber-qtd2.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
